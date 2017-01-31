8f0cfbd25cea6b73fe8d53c255c5bee81717efc5
ADERANT_AP\chuan.wu <chuan1994@hotmail.com>
2017-01-31 16:25:33 +1300

    Dice assets added in and modified params to be more suitable for use

--- HelloWorld/Assets/Scripts/IdFetcher.cs
+++ HelloWorld/Assets/Scripts/IdFetcher.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class IdFetcher : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Start () {
-		    
+    string url = "http://wolfewylie.com/cgi-bin/jeopardy.py?callback=?";
+    // Use this for initialization
+    void Start () {
+        DummyPostRequest("japan");
 	}
-	
+	    
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void DummyPostRequest(string country) {
+        WWWForm form = new WWWForm();
+        form.AddField("question", country);
+
+        WWW www = new WWW(url, form);
+        StartCoroutine(WaitFor(www));
+    }
+
+    IEnumerator WaitFor(WWW www) {
+        yield return www;
+
+        if (www.error == null)
+        {
+            Debug.Log(www.text);
+        }
+        else {
+            Debug.Log(www.error.ToString());
+        }
+    }
+
+
 }
